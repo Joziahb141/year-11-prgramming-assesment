a1f5920d16f3fbe431a56497346d23070ef9ef19
Josiah Beale <JBeale@stu.kerikerihigh.ac.nz>
2021-07-02 11:54:09 +1200

    forgot what i did to it but saving it so i can acess it from other places

--- quiz/quiz assesment.py
+++ quiz/quiz assesment.py
@@ -1,6 +1,21 @@
 import requests
+from quiz_class import user1
+print(user1)
 
-response = requests.get("http://api.open-notify.org/astros.json")
+questions = response.json()['results']
+reponse = requests.get("https://opentdb.com/api.php?amount=10&category=29&type=multiple")
+def get_integer_input(message, error, low, high):
+    while True:
+        try:
+            inpt = int(input(message))
+            if low < inpt < high or low == inpt or high == inpt:
+                return inpt
+            else:
+                print(error)
+        except ValueError:
+            print(error)
 
-for astronaut in response.json()['people']:
-    print('name', astronaut['name'])
+user_choice = get_int_input 
+def cheak_anwser():
+  for i ,question in enumerate(questions):
+    print("")
